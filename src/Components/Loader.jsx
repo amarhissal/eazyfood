3269fe2a7201e4eaf1f86dd7dc67c459a8b59a20
amarhissal <108334744+amarhissal@users.noreply.github.com>
2024-08-19 16:46:51 +0530

    Add files via upload

---
+++ src/Components/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import './Loader.css'; // Import CSS file for styling
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      {/* Spinner animation */}
+    </div>
+  );
+};
+
+export default Loader;
